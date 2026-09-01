d85300cada7b6194021f61390135fe6ca68f9630
Vansh Rajput <vr588424@gmail.com>
2026-09-01 19:57:41 +0530

    Updated profile and settings screens

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -2,10 +2,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 import 'auth_pages/login_screen.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
+
   @override
   State<SettingScreen> createState() => _SettingScreenState();
 }
@@ -14,23 +16,29 @@ class _SettingScreenState extends State<SettingScreen> {
   static const Color _backgroundColor = Color(0xFF111111);
   static const Color _cardColor = Color(0xFF1C1C1C);
   static const Color _accentColor = Color(0xFFFF8A00);
+
   final _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: _backgroundColor,
+
       appBar: AppBar(
         backgroundColor: _backgroundColor,
         surfaceTintColor: Colors.transparent,
         scrolledUnderElevation: 0,
         elevation: 0,
         centerTitle: true,
+
         leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(CupertinoIcons.back),
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(
+            CupertinoIcons.back,
+            color: Colors.white,
+          ),
         ),
+
         title: Column(
           children: [
             Text(
@@ -38,22 +46,27 @@ class _SettingScreenState extends State<SettingScreen> {
               style: GoogleFonts.poppins(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
-                color: Colors.orange,
+                color: _accentColor,
               ),
             ),
-            SizedBox(width: 110, child: Divider(color: Colors.white)),
+            Container(
+              width: 75,
+              height: 1,
+              color: Colors.white,
+            ),
           ],
         ),
-        actionsPadding: EdgeInsets.only(right: 24, bottom: 10),
       ),
-      backgroundColor: _backgroundColor,
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
+
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // _bodyDataCard(),
-            SizedBox(height: 30),
+            _bodyDataCard(),
+
+            const SizedBox(height: 30),
+
             Row(
               children: [
                 Container(
@@ -63,12 +76,16 @@ class _SettingScreenState extends State<SettingScreen> {
                     color: _accentColor.withValues(alpha: 0.12),
                     borderRadius: BorderRadius.circular(10),
                   ),
-                  child: Icon(Icons.person, color: _accentColor),
+                  child: const Icon(
+                    Icons.person,
+                    color: _accentColor,
+                  ),
                 ),
 
                 const SizedBox(width: 11),
+
                 Text(
-                  "Account",
+                  'Account',
                   style: GoogleFonts.poppins(
                     color: Colors.white,
                     fontSize: 16,
@@ -77,32 +94,39 @@ class _SettingScreenState extends State<SettingScreen> {
                 ),
               ],
             ),
-            SizedBox(height: 12),
-            Center(
-              child: Container(
-                width: MediaQuery.of(context).size.width / 1.2,
-                height: 55,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(34),
+
+            const SizedBox(height: 12),
+
+            Container(
+              width: double.infinity,
+              height: 55,
+              decoration: BoxDecoration(
+                color: Colors.red.withValues(alpha: 0.12),
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(
+                  color: Colors.red.withValues(alpha: 0.2),
                 ),
-                child: TextButton(
-                  onPressed: _logout,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.logout_rounded, color: Colors.white, size: 20),
-                      SizedBox(width: 4),
-                      Text(
-                        "Log Out",
-                        style: GoogleFonts.poppins(
-                          fontSize: 16,
-                          fontWeight: FontWeight(600),
-                          color: Colors.white,
-                        ),
+              ),
+              child: TextButton(
+                onPressed: _logout,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.logout_rounded,
+                      color: Colors.redAccent,
+                      size: 20,
+                    ),
+                    const SizedBox(width: 6),
+                    Text(
+                      'Log Out',
+                      style: GoogleFonts.poppins(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.redAccent,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
@@ -130,14 +154,18 @@ class _SettingScreenState extends State<SettingScreen> {
           ),
           content: Text(
             'Are you sure you want to logout?',
-            style: GoogleFonts.poppins(color: Colors.grey.shade400),
+            style: GoogleFonts.poppins(
+              color: Colors.grey.shade400,
+            ),
           ),
           actions: [
             TextButton(
               onPressed: () => Navigator.pop(context, false),
               child: Text(
                 'Cancel',
-                style: GoogleFonts.poppins(color: Colors.grey.shade400),
+                style: GoogleFonts.poppins(
+                  color: Colors.grey.shade400,
+                ),
               ),
             ),
             TextButton(
@@ -156,15 +184,116 @@ class _SettingScreenState extends State<SettingScreen> {
     );
 
     if (confirmed != true) return;
+
     await _auth.signOut();
+
     if (!mounted) return;
+
     Navigator.pushAndRemoveUntil(
       context,
-      MaterialPageRoute(builder: (_) => const LoginScreen()),
-      (_) => false,
+      MaterialPageRoute(
+        builder: (_) => const LoginScreen(),
+      ),
+          (_) => false,
     );
   }
 
+  Widget _bodyDataCard() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 6),
+      decoration: BoxDecoration(
+        color: _cardColor,
+        borderRadius: BorderRadius.circular(24),
+        border: Border.all(
+          color: Colors.white.withValues(alpha: 0.04),
+        ),
+      ),
+      child: Column(
+        children: [
+          ListTile(
+            onTap: () {},
+            leading: const Icon(
+              CupertinoIcons.folder_solid,
+              color: Colors.grey,
+            ),
+            title: Text(
+              'Preferences',
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontSize: 15,
+              ),
+            ),
+            trailing: const Icon(
+              CupertinoIcons.forward,
+              color: Colors.grey,
+              size: 20,
+            ),
+          ),
 
+          const Divider(),
 
+          ListTile(
+            onTap: () {},
+            leading: const Icon(
+              Icons.lock,
+              color: Colors.grey,
+            ),
+            title: Text(
+              'Privacy',
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontSize: 15,
+              ),
+            ),
+            trailing: const Icon(
+              CupertinoIcons.forward,
+              color: Colors.grey,
+            ),
+          ),
+
+          const Divider(),
+
+          ListTile(
+            onTap: () {},
+            leading: const Icon(
+              Icons.support_agent,
+              color: Colors.grey,
+            ),
+            title: Text(
+              'Support',
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontSize: 15,
+              ),
+            ),
+            trailing: const Icon(
+              CupertinoIcons.forward,
+              color: Colors.grey,
+            ),
+          ),
+
+          const Divider(),
+
+          ListTile(
+            onTap: () {},
+            leading: const Icon(
+              CupertinoIcons.doc_text_fill,
+              color: Colors.grey,
+            ),
+            title: Text(
+              'Legal',
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontSize: 15,
+              ),
+            ),
+            trailing: const Icon(
+              CupertinoIcons.forward,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
